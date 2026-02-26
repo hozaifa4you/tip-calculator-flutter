3105b82660e928bb53fe208dbc7b554be7aba6c0
Hozaifa <hozaifa4you@gmail.com>
2026-02-26 09:33:51 +0600

    Desing split

--- lib/main.dart
+++ lib/main.dart
@@ -39,23 +39,75 @@ class _UTipState extends State<UTip> {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Container(
-            decoration: BoxDecoration(
-              color: theme.colorScheme.primaryFixedDim,
-              borderRadius: BorderRadius.circular(8.0),
-            ),
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              children: [
-                Text("Total per Person", style: style),
-                Text(
-                  "Price: \$9.99",
-                  style: style.copyWith(
-                    fontSize: theme.textTheme.displaySmall?.fontSize,
-                    color: theme.colorScheme.onPrimary,
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              decoration: BoxDecoration(
+                color: theme.colorScheme.primaryFixedDim,
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  Text("Total per Person", style: style),
+                  Text(
+                    "Price: \$9.99",
+                    style: style.copyWith(
+                      fontSize: theme.textTheme.displaySmall?.fontSize,
+                      color: theme.colorScheme.onPrimary,
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(5),
+                border: Border.all(color: theme.colorScheme.primary, width: 2),
+              ),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        prefixIcon: Icon(Icons.attach_money),
+                        labelText: "Bill Amount",
+                        hintText: "Enter bill amount",
+                      ),
+                      keyboardType: TextInputType.numberWithOptions(
+                        decimal: true,
+                      ),
+                      onChanged: (String value) {
+                        print("Price: $value");
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text("Split", style: theme.textTheme.titleMedium),
+                        Row(
+                          children: [
+                            IconButton(
+                              onPressed: () {},
+                              icon: Icon(Icons.remove),
+                            ),
+                            Text("2", style: theme.textTheme.titleMedium),
+                            IconButton(onPressed: () {}, icon: Icon(Icons.add)),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ],
